6a09d23806fa909db26bb704f585c4e0b3c2534f
Óscar García <geoscar1100@gmail.com>
2026-01-13 19:35:24 +0100

    modified and added new stylesÂ

--- src/services/pokemon.ts
+++ src/services/pokemon.ts
@@ -1,16 +1,13 @@
 import type { ParamValue } from "next/dist/server/request/params";
 
-const API_URL: string | undefined =
-  `${process.env.NEXT_PUBLIC_API_URL}`;
+const API_URL: string | undefined = `${process.env.NEXT_PUBLIC_API_URL}`;
 
 export const getPokemon = async (pokemonName: ParamValue) => {
-  
-    const data = await fetch(`${API_URL}/${pokemonName}` || "");
-    console.log(data);
-    return data.json();
+  const data = await fetch(`${API_URL}/${pokemonName}` || "");
+  return data.json();
 };
 
 export const getPokemons = async () => {
-    const data = await fetch(`${API_URL}?limit=100` || "");
-    return data.json();
+  const data = await fetch(`${API_URL}?limit=100` || "");
+  return data.json();
 };
